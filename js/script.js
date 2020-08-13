a2cf301baf4282eaee9243a68f5ecb2b57ac55b5
Jr1213 <mohammed.jr.hammad@gmail.com>
2020-08-13 16:29:36 +0200

    fix navbar responsive behiver

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const humberger = document.querySelector('.wrapper .humberger');
 const sideMenu = document.querySelector('.nav-list');
-
+const navLink = document.querySelectorAll('.link');
 
 humberger.addEventListener('click', function(){
     sideMenu.classList.toggle("activ-m");
     humberger.classList.toggle('activ-h');
+    navLink.forEach(element => {
+        element.addEventListener('click',()=>{
+           sideMenu.classList.remove('activ-m') 
+        })
+     });
 
 })
+
